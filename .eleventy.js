b39a476f838dac56dca1f82bd97d3c6575db543e
hsiangting49 <157249687+hsiangting49@users.noreply.github.com>
2024-03-05 02:18:27 +0000

    v3

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,7 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("styles");
-  
+  eleventyConfig.addPassthroughCopy("utilities.js");
 };
 
 
